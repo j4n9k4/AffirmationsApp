04bc50140bd5b561ec298a1bbd25f52e0314474c
j4n9k4 <janukananayakkara@gmail.com>
2025-02-16 23:31:13 +0530

    Created the affirmation card

--- app/src/main/java/com/example/affirmationsapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationsapp/MainActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.affirmationsapp.model.Affirmation
 import com.example.affirmationsapp.ui.theme.AffirmationsAppTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,4 +47,33 @@ class MainActivity : ComponentActivity() {
 fun AffirmationsApp()
 {
 
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier)
+{
+    Card(modifier = modifier) {
+        Column(modifier = modifier) {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun AffirmationCardPreview()
+{
+    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
 }
